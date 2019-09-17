621a5c2d798906126b962e73cc1473fbda5c3dbf
sourabh singh <sourabhsingh01998@gmail.com>
2019-09-17 11:09:36 +0530

    hello

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,4 +1,6 @@
 document.getElementById('add').addEventListener('click',function (){
 	var value=document.getElementById('item').value;
-	console.log(`This is working the value is ${value}`);
+	if(value){
+		
+	}
 });
